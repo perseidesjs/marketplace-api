61294f075958651cb7b06d6d36f1cde93998da32
adevinwild <68734155+adevinwild@users.noreply.github.com>
2024-05-14 20:59:48 +0200

    fix: index.d.ts types

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,11 +4,13 @@ import type { ShippingProfile } from './models/shipping-profile'
 declare module '@medusajs/medusa/dist/models/product' {
     interface Product {
         store_id?: string
+        store?: Store
     }
 }
 
 declare module '@medusajs/medusa/dist/models/shipping-profile' {
     interface ShippingProfile {
         store_id?: string
+        store?: Store
     }
 }
