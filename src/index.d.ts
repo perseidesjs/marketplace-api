ac64a82d394553275d78995f8be1d1a05c4c4ca9
adevinwild <68734155+adevinwild@users.noreply.github.com>
2024-05-14 21:01:39 +0200

    fix : index.d.ts types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,9 @@
 import type { Product } from './models/product'
+import type { Store } from './models/store'
 
 declare module '@medusajs/medusa/dist/models/product' {
     interface Product {
         store_id?: string
+        store?: Store
     }
 }
